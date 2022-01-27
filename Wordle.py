5e37f40b9e2fc9aa078868bfae2aeceafe7f2cfc
Tomás <tomasbossi97@gmail.com>
2022-01-27 07:55:18 -0300

    Corregir archivo y script y mejorar prints

--- Wordle.py
+++ Wordle.py
@@ -1,14 +1,14 @@
-import datetime 
+import datetime
 import random
 import string
-import os 
+import os
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 # 1 Setear parámetros
 abecedario = list(string.ascii_lowercase+'ñ') # excluye caracteres extraños/decorados/numéricos
 nLetras    = 5 # largo de la palabra target y de todas las palabras que podrán ser input del jugador
 nIntentos  = 6 # n° de intentos permitidos, incluyendo el primero
-dificil    = False # Flase baja dificultad, True alta dificultad (requiere usar todas las pistas disponibles en adivinaciones sucesivas) (opcional)
+dificil    = True # False baja dificultad, True alta dificultad (requiere usar todas las pistas disponibles en adivinaciones sucesivas) (opcional)
 diaria     = False # (opcional)
 # Si diaria = True, se fija random.seed() con un identificador único del día actual. La palabra target es invariante durante el día entero
 # Si diaria = False, la palabra target es elegida al azar en cada ejecución, independientemente de la fecha
@@ -63,20 +63,25 @@ def palabraTarget(subdicc):
 #                0: no tan bien ubicada, 
 #               -1: letra no presente en target
 def comparar(target, input):
-    comparacion = []
+    comparacion = [-1] * len(target)
     rep = {letra: target.count(letra) for letra in target}
-    for i, letraInput in enumerate(input):
-        ubicacion = -1
-        if letraInput in target and rep[letraInput]:
-            ubicacion = 1 - (letraInput != target[i])
+
+    for i, letraInput in enumerate(input): # 1er loop, para letras bien ubicadas (verde, 1)
+        if letraInput == target[i]:
             rep[letraInput] -= 1
-        comparacion.append(ubicacion)
+            comparacion[i] = 1
+    
+    for i, letraInput in enumerate(input): # 2do loop, para letras no tan bien ubicadas (amarillo, 0)
+        if letraInput in target and rep[letraInput] and comparacion[i] != 1:
+            rep[letraInput] -= 1
+            comparacion[i] = 0
+
     return comparacion
 
 # print(comparar('xxxxx', 'xxxxx'), # [ 1,  1,  1,  1,  1]
 #       comparar('xxxxx', 'yyyyy'), # [-1, -1, -1, -1, -1]
-#       comparar('knoll', 'lobos'), # [ 0,  0, -1, -1, -1]
-#       comparar('casio', 'patos')) # [-1,  1, -1,  0,  0]
+#       comparar('xxyzz', 'zxxwx'), # [ 0,  1,  0, -1, -1]
+#       comparar('xyyyx', 'xxyxx')) # [ 1, -1,  1, -1,  1]
 
 # 10 Generar output intuitivo en consola a partir de la comparación entre el target y el input del usuario
 def mostrarPistas(input, comparacion):
@@ -85,18 +90,19 @@ def mostrarPistas(input, comparacion):
         pistas += '+'*(ubicacion==1) + '-'*(ubicacion==0) + '_'*(ubicacion==-1)
     print(input)
     print(pistas)
+    return input, pistas
 
 # mostrarPistas('patos', comparar('casio', 'patos')) # patos
 #                                                    # _+_--
 
 # 11, 13 (optativo), 14 (optativo) Jugar un intento
 def jugarIntento(target, subdicc, abecedario, repetidas = []):
-    input         = pedirPalabra()
-    inputValido   = esValido(input, subdicc, abecedario, repetidas)
-    errores       = ['Tu palabra no es del largo correcto.',
-                     'Tu palabra tiene algún caracter extraño.',
-                     'Tu palabra no está en el diccionario.',
-                     'Ya probaste esa palabra!'] # Lista de errores jerarquizados
+    input       = pedirPalabra()
+    inputValido = esValido(input, subdicc, abecedario, repetidas)
+    errores     = ['Tu palabra no es del largo correcto.',
+                   'Tu palabra tiene algún caracter extraño.',
+                   'Tu palabra no está en el diccionario.',
+                   'Ya probaste esa palabra!'] # Lista de errores jerarquizados
     while not all(inputValido):
         print(errores[inputValido.index(False)], 'Intentá nuevamente.')
         input       = pedirPalabra()
@@ -131,7 +137,7 @@ def semillaDiaria():
     return id
 
 def jugarPartida(nLetras, nIntentos, diccionario, abecedario, diaria):
-    if diaria: random.seed(semillaDiaria())
+    if diaria:  random.seed(semillaDiaria())
     subdicc   = generarSubdiccionario(diccionario, abecedario, nLetras)
     target    = palabraTarget(subdicc) 
     repetidas = []
@@ -142,10 +148,8 @@ def jugarPartida(nLetras, nIntentos, diccionario, abecedario, diaria):
         victoria = jugarIntento(target, subdicc, abecedario, repetidas)
         i += 1
 
-    if victoria:
-        print('Ganaste en '+str(i)+' intentos!')
-    else:
-        print('F. La palabra era '+target+'.')
+    if victoria: print('Ganaste en '+str(i)+' intentos!')
+    else: print('F. La palabra era '+target+'.')
     return victoria
 
 # 16 (opcional) definir usaPistas() y modificar esValido(), jugarIntento() y jugarPartida() para permitir la elección de dificultad (False baja, True alta), agregar variable dificil en # 1
@@ -153,63 +157,95 @@ def jugarPartida(nLetras, nIntentos, diccionario, abecedario, diaria):
 # Las letras en posicion correcta deben permanecer en posicion
 # Las letras en posicion no tan correcta deben utilizarse en como minimo igual cantidad de veces que la cantidad de veces que aparecen no tan bien ubicadas en la pista
 # Por lo tanto se requerirá una constancia de la posición y un contador de letras, de forma analoga a lo hecho para comparar()
-def usaPistas(input, prevInput, prevComparacion):
-    comparacionInputs = comparar(input, prevInput)
-    prevInputFiltrado = [letra for i, letra in enumerate(prevInput) if prevComparacion[i] in [0, 1]]
-    letrasObligadas   = {letra: prevInput.count(letra) for letra in prevInputFiltrado}
-    for i, ubicacion in enumerate(prevComparacion):
-        if prevInput[i] in letrasObligadas.keys() and letrasObligadas[prevInput[i]]:
-            if (ubicacion == 1 and comparacionInputs[i] == 1) or (ubicacion == 0 and comparacionInputs[i] in [0, 1]):
-                letrasObligadas[prevInput[i]] -= 1
-    return all([letrasObligadas[letra] == 0 for letra in letrasObligadas.keys()])
+def contarLetrasResaltadas(input, comparacion, tipo):
+    inputFiltrado = [letra for i, letra in enumerate(input) if comparacion[i] == tipo]
+    return {letra: inputFiltrado.count(letra) for letra in inputFiltrado}
 
-# print(usaPistas('amuse', 'audio', [ 0,  1, -1, -1, -1]), # False
-#       usaPistas('muase', 'audio', [ 0,  1, -1, -1, -1]), # True
-#       usaPistas('uadio', 'audio', [-1,  1,  1,  1,  1]), # False
-#       usaPistas('aaaxx', 'fuaaa', [-1, -1,  1,  0,  0]), # True
-#       usaPistas('xaaxx', 'fuaaa', [-1, -1,  1,  0,  0])) # False
+def usaPistas(target, input, prevInput):
+    pistas        = comparar(target, prevInput)
+    letrasFijas   = contarLetrasResaltadas(prevInput, pistas, 1)
+    letrasMoviles = contarLetrasResaltadas(prevInput, pistas, 0)
+    for i, ubicacion in enumerate(pistas):
+        if ubicacion == 1:
+            if input[i] == prevInput[i]:
+                letrasFijas[input[i]] -= 1
+        elif input[i] in letrasMoviles.keys() and letrasMoviles[input[i]]:
+            letrasMoviles[input[i]] -= 1
+    return not any(list(letrasFijas.values()) + list(letrasMoviles.values()))
 
-def esValido(input, subdicc, abecedario, repetidas, dificil = 0, prevInput = '', comparacion = []):
-    return esFormable(input, abecedario, len(subdicc[0])) + [(input in subdicc), input not in repetidas] + [usaPistas(input, prevInput, comparacion)] * dificil
+print(usaPistas('yyyyy', 'xxxxx', 'xxxxx'), # True
+      usaPistas('zxvyu', 'ywxzw', 'yxwww'), # False
+      usaPistas('yyyyy', 'yyxyy', 'xxyxx'), # False
+      usaPistas('zxywv', 'wzxyw', 'xyxzw')) # True
 
-def jugarIntento(target, subdicc, abecedario, repetidas, dificil = 0, prevInput = '', comparacion = []):
-    input         = pedirPalabra()
-    inputValido   = esValido(input, subdicc, abecedario, repetidas, dificil, prevInput, comparacion)
-    errores       = ['Tu palabra no es del largo correcto.',
-                     'Tu palabra tiene algún caracter extraño.',
-                     'Tu palabra no está en el diccionario.',
-                     'Ya probaste esa palabra!',
-                     'Tenés que usar todas las pistas disponibles!'] # Lista de errores jerarquizados
+def esValido(input, subdicc, abecedario, repetidas, target, dificil):
+    valido = [*esFormable(input, abecedario, len(subdicc[0])), input in subdicc]
+    if repetidas: 
+        valido.append(input not in repetidas)
+        if dificil: 
+            valido.append(usaPistas(target, input, repetidas[-1]))
+    return valido
+
+def jugarIntento(target, subdicc, abecedario, repetidas, dificil):
+    input       = pedirPalabra()
+    inputValido = esValido(input, subdicc, abecedario, repetidas, target, dificil)
+    errores     = ['Tu palabra no es del largo correcto.',
+                   'Tu palabra tiene algún caracter extraño.',
+                   'Tu palabra no está en el diccionario.',
+                   'Ya probaste esa palabra!',
+                   'Tenés que usar todas las pistas disponibles!'] # Lista de errores jerarquizados
     while not all(inputValido):
         print(errores[inputValido.index(False)], 'Intentá nuevamente.')
         input       = pedirPalabra()
-        inputValido = esValido(input, subdicc, abecedario, repetidas, dificil, prevInput, comparacion)
+        inputValido = esValido(input, subdicc, abecedario, repetidas, target, dificil)
     repetidas.append(input) # repetidas es modificada in-place
     comparacion = comparar(target, input)
     mostrarPistas(input, comparacion)
-    return input, comparacion
+    return comparacion
 
-def jugarPartida(nLetras, nIntentos, diccionario, abecedario, diaria = True, dificil = False):
-    if diaria: random.seed(semillaDiaria())
-    subdicc     = generarSubdiccionario(diccionario, abecedario, nLetras)
-    target      = palabraTarget(subdicc)
-    repetidas   = []
-    prevInput   = nLetras * ' '
-    comparacion = nLetras * [-1]
-    victoria    = False
+def jugarPartida(nLetras, nIntentos, diccionario, abecedario, dificil = False, diaria = True):
+    if diaria:  random.seed(semillaDiaria())
+    subdicc   = generarSubdiccionario(diccionario, abecedario, nLetras)
+    target    = palabraTarget(subdicc)
+    repetidas = []
+    victoria  = False
     i = 0
     while not victoria and i < nIntentos:
         print('#### Intento '+str(i+1)+' ####')
-        prevInput, comparacion = jugarIntento(target, subdicc, abecedario, repetidas, dificil, prevInput, comparacion)
-        victoria = all([ubicacion == 1 for ubicacion in comparacion])
+        comparacion = jugarIntento(target, subdicc, abecedario, repetidas, dificil)
+        victoria    = all([ubicacion == 1 for ubicacion in comparacion])
         i += 1
 
-    if victoria:
-        print('Ganaste en '+str(i)+' intentos!')
-    else:
-        print('F. La palabra era '+target+'.')
+    if victoria: print('Ganaste en '+str(i)+' intentos!')
+    else: print('F. La palabra era '+target+'.')
     return victoria
 
-jugarPartida(nLetras, nIntentos, diccionario, abecedario, dificil)
-
 # 17 Go off
+
+# Mejores impresiones en consola
+
+def imprimirHistorial(target, repetidas):
+    print('')
+    for input in repetidas:
+        comparacion = comparar(target, input)
+        mostrarPistas(input, comparacion)
+    print('')
+    return comparacion
+
+def jugarIntento(target, subdicc, abecedario, repetidas, dificil):
+    input       = pedirPalabra()
+    inputValido = esValido(input, subdicc, abecedario, repetidas, target, dificil)
+    errores     = ['Tu palabra no es del largo correcto.',
+                   'Tu palabra tiene algún caracter extraño.',
+                   'Tu palabra no está en el diccionario.',
+                   'Ya probaste esa palabra!',
+                   'Tenés que usar todas las pistas disponibles!'] # Lista de errores jerarquizados
+    while not all(inputValido):
+        print(errores[inputValido.index(False)], 'Intentá nuevamente.')
+        input       = pedirPalabra()
+        inputValido = esValido(input, subdicc, abecedario, repetidas, target, dificil)
+    repetidas.append(input) # repetidas es modificada in-place
+    comparacion = imprimirHistorial(target, repetidas)
+    return comparacion
+
+# jugarPartida(nLetras, nIntentos, diccionario, abecedario, dificil, diaria)
